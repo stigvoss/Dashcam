b5cf041b19757b495debdedf18675b458f58757b
Stig Voss <stig.voss@gmail.com>
2016-12-02 21:47:17 +0200

    Added Module for Rotate an image 180 degrees

---
+++ Flip/Module.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Dashcam.Common;
+using System.Drawing;
+
+namespace Flip
+{
+    public class Module : Library.Dashcam.Extensibility.Module
+    {
+        private const string MODULE_NAME = "Flip";
+
+        public override string Name
+        {
+            get
+            {
+                return MODULE_NAME;
+            }
+        }
+
+        public override void Execute(FrameInfo frameInfo)
+        {
+            Bitmap bitmap = frameInfo.Frame;
+
+            bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+        }
+    }
+}
